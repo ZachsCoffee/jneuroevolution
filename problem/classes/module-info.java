329e82b8333e7f19ea970e7e0570a9ba5ae02049
33447142+ZachsCoffee <33447142+ZachsCoffee@users.noreply.github.com>
2022-08-16 18:19:10 +0300

    move to correct location

--- problem/classes/module-info.java
+++
@@ -1,8 +0,0 @@
-module problem {
-    exports executors;
-    requires machine_learning;
-    requires neuroevolution_gui;
-    requires evolution_algorithms;
-    requires utils;
-    requires neuroevolution;
-}
